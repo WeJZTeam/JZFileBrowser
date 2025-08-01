25cdb81cbf66f7d5e2a3f20724d567cccc0574c0
TeamJZ <zaxx.sp.c1@gmail.com>
2025-08-01 15:43:40 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 pip install --upgrade pip
 pip install flask werkzeug
-chmod +x JZFileBrowser/main
 # Run the application
 echo -e "${GREEN}"
 echo "Starting JZFileBrowser..."
 echo -e "Access at: ${BLUE}http://localhost:5000${GREEN}"
 echo "Press Ctrl+C to stop"
 echo -e "${NC}"
-./JZFileBrowser/main
+python JZFileBrowser/main.py
